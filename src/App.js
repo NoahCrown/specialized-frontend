f31ea88baab46d83d51c63246c9dd38acf9a5568
raphael@offshorly.com <raphael@offshorly.com>
2024-02-27 19:15:56 +0800

    modified config file

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import axios from 'axios'
 import { useCandidate } from './store/Context';
 import { ToastContainer } from 'react-toastify';
 import ModalLoader from "./components/ModalLoader";
-import { API_URL } from "./config";
+import { REACT_APP_API_URL } from "./config";
 import PushToBullorn from "./components/Output/PushToBullorn";
 
 
@@ -18,7 +18,7 @@ function App() {
   useEffect(() => {
     async function fetchData() {
       try {
-        const response = await axios.get(`${API_URL}/process_data`);
+        const response = await axios.get(`${REACT_APP_API_URL}/process_data`);
         setAllData(response.data);
         console.log(response.data);
       } catch (error) {
