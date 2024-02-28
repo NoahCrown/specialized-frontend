f82bef37297a01d47c79de40152f1262a40df024
raphael@offshorly.com <raphael@offshorly.com>
2024-02-28 09:32:58 +0800

    added process.env as API url

--- src/App.js
+++ src/App.js
@@ -7,18 +7,17 @@ import axios from 'axios'
 import { useCandidate } from './store/Context';
 import { ToastContainer } from 'react-toastify';
 import ModalLoader from "./components/ModalLoader";
-import { API_URL } from "./config";
 import PushToBullorn from "./components/Output/PushToBullorn";
 
-
-
+const REACT_APP_API_URL = process.env.REACT_APP_API_URL
+axios.defaults.baseURL = process.env.REACT_APP_API_URL
 
 function App() {
   const { setAllData, isLoading, isPushingToBullhorn } = useCandidate();
   useEffect(() => {
     async function fetchData() {
       try {
-        const response = await axios.get(`${API_URL}/process_data`);
+        const response = await axios.get(`${REACT_APP_API_URL}/api/process_data`);
         setAllData(response.data);
         console.log(response.data);
       } catch (error) {
